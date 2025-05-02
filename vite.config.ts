ba1f391ec78d6f24ebd348f8bc744617ad9d43bd
Vidit wanjari <98236153+Vidit72wanjari@users.noreply.github.com>
2025-05-02 21:25:29 +0530

    Initial commit for WeatherMLPrediction

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/WeatherML/', // <- very important!
+  base: '/WeatherML/', // Must exactly match your repo name
 })
